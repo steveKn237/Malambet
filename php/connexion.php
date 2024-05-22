44c2037344e9d702a2827a93d8d913402e82f93b
Yugz-prog <157105564+Yugz-prog@users.noreply.github.com>
2024-05-22 15:23:18 +0200

    Création de la fonction de création de paris
    et changement du mdp en hash

--- php/connexion.php
+++ php/connexion.php
@@ -1,15 +1,25 @@
 <?php
+require_once("crud.php");
+
+// createBet(null,"fesfsef", 1, 2, 20, 1.40);
+
 $submit = filter_input(INPUT_POST, "submit");
 
 if(isset($submit))
 {
+
     $uid = filter_input(INPUT_POST, "username");
     $pwd = filter_input(INPUT_POST, "password");
-    
 
-    
+    $result = createUser($uid,$pwd);
+
+    if(is_bool($result))
+        echo "success";
+    else{
+        echo $result;
+    }
 };
-$uid = null
+$submit = null;
 ?>
 
 <!DOCTYPE html>
@@ -26,6 +36,9 @@ $uid = null
         <label for="password">Password</label>
         <input type="password" id="Pwd" name="password">
         <input type="submit" name="submit">
+    </form>
+    <form action="" method="post">
+        <label for=""></label>
     </form>
 </body>
 </html>
--- php/crud.php
+++ php/crud.php
@@ -1,14 +1,71 @@
 <?php
 require_once("pdo.php");
 
-function userExist($uid){
-    PDO->prepare("SELECT ");
-    
+function userExist($name){
+    $stmt = PDO->prepare("SELECT UserID, Nom FROM Utilisateurs");
+    $stmt->execute();
+    $users = $stmt->fetchAll();
+    foreach($users as $user)
+    {
+        if($name == $user['Nom'])
+            return $user['UserID'];
+    }
+    return false;
+    $stmt->closeCursor();
 };
 
-function createUser($uid, $pwd){
-    if(userExist($uid))
-        return false;
-    PDO->exec("INSERT INTO Utilisateurs(Nom, Mdp) VALUES('$uid', '$pwd')");
+
+
+function createUser($name, $pwd){
+    if(userExist($name))
+        return "Cet utilisateur existe déjà";
+
+    $pwd = hash("sha256", $pwd);
+    $stmt = PDO->prepare("INSERT INTO Utilisateurs(Nom, Mdp_hash) VALUES(?, ?)");
+    $stmt->execute([$name, $pwd]);
+    $stmt->closeCursor();
+    return true;
 };
+
+
+function findTeamID($name){
+
+    $stmt = PDO->prepare("SELECT EquipeID FROM Equipes WHERE NomEquipe = $name");
+
+    $stmt->execute();
+    $equipe = $stmt->fetchAll();
+
+    $stmt->closeCursor();
+
+    return $equipe["EquipeID"];
+}
+
+
+function createBet($uid = null, $name = null, $matchID, $equipeID, $montant, $cote)
+{
+    if($uid === null && $name === null)
+        return false;
+
+    if($uid != null){
+        try{
+            $stmt = PDO->prepare("INSERT INTO Paris(UserPariID, MatchPariID, EquipeChoisie, Montant_mise, Cote, Statut_pari) VALUES(:uid, :matchID, :equipeID, :montant, :cote, 'en cours') ");
+            $stmt->bindParam("uid", $uid, PDO::PARAM_INT);
+            $stmt->bindParam("matchID", $matchID, PDO::PARAM_INT);
+            $stmt->bindParam("equipeID", $equipeID, PDO::PARAM_INT);
+            $stmt->bindParam("montant", $montant, PDO::PARAM_INT);
+            $stmt->bindParam("cote", $cote, PDO::PARAM_STR);
+            $stmt->execute();
+        }
+        catch(PDOException){
+            return false;
+        }
+    }
+    else if($name != null){
+        $stmt = PDO->prepare("SELECT UserID FROM Utilisateurs WHERE Nom = '".$name."'");
+        $stmt->execute();
+        $uid = $stmt->fetchAll();
+        $stmt->closeCursor();
+        createBet($uid,null,$matchID,$equipeID,$montant,$cote);
+    }
+}
 ?>
